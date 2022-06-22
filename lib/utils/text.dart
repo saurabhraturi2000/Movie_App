dd4a97ac1fbd61bfa43a43a42f703b9d05b46f59
saurabhraturi2000 <saurabhraturi999@gmail.com>
2022-06-22 13:42:54 +0530

    ola

---
+++ lib/utils/text.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class modifiedText extends StatelessWidget {
+  final String text;
+  final Color color;
+  final double size;
+  const modifiedText(
+      {Key? key, required this.text, required this.color, required this.size})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: GoogleFonts.breeSerif(color: color, fontSize: size),
+    );
+  }
+}
---
+++ lib/widgets/trending.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class TrendingMovies extends StatelessWidget {
+  final List trending;
+  const TrendingMovies({Key? key, required this.trending}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(10),
+      child: Column(children: [Text('')]),
+    );
+  }
+}
